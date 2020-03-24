382c6317ec8e883eecf36df833b249533eb329f7
nriserEG <nriser@evergage.com>
2020-03-24 12:42:10 -0700

    Add json files for template names

--- home-hero-promo/client.js
+++ home-hero-promo/client.js
@@ -1,22 +1,22 @@
 (function() {
 
-    function apply(context, template, render) {
-      render(context);
-    }
-  
-    function reset(context, template) {
-      var inlineMsg = Evergage.cashDom(".hh-inline");
-      if (inlineMsg) inlineMsg.remove();
-    }
+  function apply(context, template, render) {
+    render(context);
+  }
 
-    function control() {
-    
-    }
+  function reset(context, template) {
+    var inlineMsg = Evergage.cashDom(".hh-inline");
+    if (inlineMsg) inlineMsg.remove();
+  }
+
+  function control() {
   
-    registerTemplate({
-      apply: apply,
-      reset: reset,
-      control: control
-    });
-    
-  })();
+  }
+
+  registerTemplate({
+    apply: apply,
+    reset: reset,
+    control: control
+  });
+  
+})();
