d7a611c25230c21c627c9a84eb946148ee83171f
nriserEG <nriser@evergage.com>
2020-04-02 12:07:15 -0700

    Bug fix - Add check for context before appending html

--- promo-infobar/client.js
+++ promo-infobar/client.js
@@ -1,16 +1,18 @@
 (function() {
 
   function apply(context, template, render) {
-    var html = template(context);
-    Evergage.cashDom("body").append(html);
-    if (context.infobarPosition.position === "top") {
-      Evergage.cashDom("body").css("margin-bottom", "0px");
-      Evergage.cashDom("body").css("margin-top", "40px");
-      Evergage.cashDom("#evg-promo-code-infobar").css("top", "0px");
-    } else {
-      Evergage.cashDom("body").css("margin-bottom", "40px");
-      Evergage.cashDom("body").css("margin-top", "0px");
-      Evergage.cashDom("#evg-promo-code-infobar").css("bottom", "0px");
+    if (context && context.infobarPosition) {
+      var html = template(context);
+      Evergage.cashDom("body").append(html);
+      if (context.infobarPosition.position === "top") {
+        Evergage.cashDom("body").css("margin-bottom", "0px");
+        Evergage.cashDom("body").css("margin-top", "40px");
+        Evergage.cashDom("#evg-promo-code-infobar").css("top", "0px");
+      } else {
+        Evergage.cashDom("body").css("margin-bottom", "40px");
+        Evergage.cashDom("body").css("margin-top", "0px");
+        Evergage.cashDom("#evg-promo-code-infobar").css("bottom", "0px");
+      }
     }
   }
 
