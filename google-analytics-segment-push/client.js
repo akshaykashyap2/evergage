ed74cd643a0d3e56df0766e2fa276d284032d86c
nriserEG <nriser@evergage.com>
2020-05-06 17:13:53 -0700

    Refactor - return boolean in payload instead of segment ids

--- google-analytics-segment-push/client.js
+++ google-analytics-segment-push/client.js
@@ -3,34 +3,16 @@
   function apply(context, template, render) {
     let userSegments = context.userSegments;
     let userDimensions = context.userDimensions;
-    // console.log(userSegments[0])
 
     if (window.ga && typeof window.ga === "function") {
-        // var segments = {
-        //     "d3jv3": false,
-        //     "K871s": false,
-        //     "gWOBI": false
-        // };
-
-        // /* #set ($segmentIds = $tools.user.get('SegmentIds', 'none')) */
-        // /* #if ($segmentIds.contains('d3jv3')) */
-        // segments.d3jv3 = true;
-        // /* #end */
-        // /* #if ($segmentIds.contains('K871s')) */
-        // segments.K871s = true;
-        // /* #end */
-        // /* #if ($segmentIds.contains('gWOBI')) */
-        // segments.gWOBI = true;
-        // /* #end */
-
-        userSegments.forEach(function(userSegment) {
-            let userDimension = 'dimension' + userDimensions.pop()
-            ga('set', userDimension, userSegment);
+        
+        userSegments.forEach(function(userSegment, i) { debugger;
+            if (userSegment) {
+                let userDimension = 'dimension' + userDimensions[i]
+                ga('set', userDimension, userSegment);
+            }
         })
- 
-        // ga('set', 'dimension1', "K871s");
-        // ga('set', 'dimension2', segments[1]);
-        // ga('set', 'dimension3', segments[2]);
+
         ga('send', {
             hitType: 'event',
             eventCategory: 'Evergage',
--- google-analytics-segment-push/template.ts
+++ google-analytics-segment-push/template.ts
@@ -1,51 +1,25 @@
 import { UserSegmentLookup, UserSegmentReference } from "common"
 
 export class GASegmentTemplate implements CampaignTemplateComponent {
-    
+
     @lookupOptions(() => new UserSegmentLookup())
     segments: UserSegmentReference[]
 
-    googleAnalyticsDimensions: string
+    @title("Google Analytics Dimensions")
+    @subtitle("Enter a comma or space delimited list of the Google Analytics dimension numbers")
+    gaDimensions: string
 
     run(context:CampaignComponentContext) {
 
-        console.log("user id is", context.user.id);
-        // console.log("this.segments is", this.segments);
-        // console.log("this.segments is", JSON.stringify(this.segments));
+        let userDimensions = this.gaDimensions ? this.gaDimensions.match(/\d+/g).map(Number) : []
 
         let userSegments = [];
         if (this.segments) {
             this.segments.forEach(function(segment) { 
-
-                // console.log("segment is", JSON.stringify(segment));
-                // console.log("segment ID is", JSON.stringify(segment.id));
-
                 let segmentJoinDate = context.user.getSegmentJoinDate(segment.id);
-            
-                console.log("segmentJoinDate", segmentJoinDate)
-
-                if (segmentJoinDate) {
-                    userSegments.push(segment.id);
-                } 
-
-                // console.log("segmentJoinDates", userSegments)
+                userSegments.push(segmentJoinDate ? true : false);
             });
         }
-        
-        // console.log("segments are", userSegments);
-        
-        // let usersegmentcheck = userSegmentRule.isUserMember(context, user) // deprecated
-        // let segmentJoinDate = context.user.getSegmentJoinDate("CZkDo")
-
-        let gaDimensions = this.googleAnalyticsDimensions;
-        let userDimensions = gaDimensions? gaDimensions.split(",").map(Number): []
-        // console.log(gaDimensionsNumbers);
-
-        // let gaDimensionsNumbers = []
-        // if (gaDimensions) {
-        //     gaDimensionsNumbers = gaDimensions.split(",").map(Number);
-        //     console.log(gaDimensionsNumbers)
-        // }
 
         return {
             userSegments: userSegments,
