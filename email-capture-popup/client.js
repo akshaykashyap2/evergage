a05844aa757e267647fc5ae7880dd1e9a3fc8c91
nriserEG <nriser@evergage.com>
2020-06-01 15:49:38 -0700

    Add comments to clientside code of Email Capture Popup template

--- email-capture-popup/client.js
+++ email-capture-popup/client.js
@@ -3,21 +3,24 @@
     function apply(context, template, render) {
         var html = template(context);
         Evergage.cashDom("body").append(html);
-      
-        Evergage.cashDom("#evg-email-capture-popup .evg-overlay, #evg-email-capture-popup .evg-close, #evg-email-capture-popup .evg-opt-out").on("click", function() {
-            Evergage.cashDom("#evg-email-capture-popup").remove();
-        });
 
-        Evergage.cashDom("#evg-email-capture-popup .evg-cta").on("click", function(event) {
-            Evergage.cashDom("#evg-email-capture-popup .evg-content").addClass("evg-hide");
-            Evergage.cashDom("#evg-email-capture-popup .evg-confirm-content").removeClass("evg-hide");
-        });
-
+        // Style config setup
         if (context.style === "Dark on Light") {
             Evergage.cashDom("#evg-email-capture-popup, #evg-email-capture-popup .evg-cta").addClass("evg-dark");
         } else {
             Evergage.cashDom("#evg-email-capture-popup, #evg-email-capture-popup .evg-cta").addClass("evg-light");
         }
+
+        // Dismisses popup
+        Evergage.cashDom("#evg-email-capture-popup .evg-overlay, #evg-email-capture-popup .evg-close, #evg-email-capture-popup .evg-opt-out").on("click", function() {
+            Evergage.cashDom("#evg-email-capture-popup").remove();
+        });
+
+        // Shows secondary confirmation panel
+        Evergage.cashDom("#evg-email-capture-popup .evg-cta").on("click", function(event) {
+            Evergage.cashDom("#evg-email-capture-popup .evg-content").addClass("evg-hide");
+            Evergage.cashDom("#evg-email-capture-popup .evg-confirm-content").removeClass("evg-hide");
+        });
     }
 
     function reset(context, template) {
